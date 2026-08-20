a6f221f6ef3dac34eed81317e07282751421a578
amanda-duong <amandaduong22@gmail.com>
2026-08-20 14:12:12 -0700

    Added live plotting of light and sound levels using matplotlib

--- python/monitor.py
+++ python/monitor.py
@@ -1,5 +1,7 @@
 import serial
 import time
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation
 
 arduino = serial.Serial("COM7", 9600)
 
@@ -9,21 +11,41 @@ times = []
 lightData = []
 soundData = []
 
-while True:
+fig, (graph1, graph2) = plt.subplots(2, 1)
+
+def update(frame):
     line = arduino.readline().decode().strip()
+
     if line.startswith("DATA"):
         parts = line.split(",")
-
         light = int(parts[1])
         sound = int(parts[2])
 
-        times.append(time.time() - startTime)
+        currentTime = time.time() - startTime
+        times.append(currentTime)
         lightData.append(light)
         soundData.append(sound)
 
-        print("Time: ")
-        print(time.time() - startTime)
+        graph1.clear()
+        graph1.plot(times, soundData)
+        graph1.set_ylim(0,300)
+        graph1.set_xlabel("Time (seconds)")
+        graph1.set_ylabel("Sound Level")
+        graph1.set_title("Live Sound Level")
+
+        graph2.clear()
+        graph2.plot(times, lightData)
+        graph2.set_ylim(0,800)
+        graph2.set_xlabel("Time (seconds)")
+        graph2.set_ylabel("Light Level (ADC)")
+        graph2.set_title("Live Light Level")
+
+
+        print("Time:", round(currentTime, 2))
         print("Light:", light)
         print("Sound:", sound)
 
+ani = FuncAnimation(fig, update, interval=100)
+plt.tight_layout()
+plt.show()
 
